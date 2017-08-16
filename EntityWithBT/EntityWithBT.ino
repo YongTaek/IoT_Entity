69ce7f6dbe1b783d78a422d4eb1122c7f659f3cf
오용택 <ka123ak1@gmail.com>
2017-08-16 15:51:03 +0900

    [08/17] Bluetooth를 이용해 요청하고 result가 true일 경우 led에 불빛이 들어오도록 함 아직 pep와의 테스트를 하지 않음

---
+++ EntityWithBT/EntityWithBT.ino
@@ -0,0 +1,53 @@
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
+
+int bluetoothTx = 5;
+int bluetoothRx = 6;
+
+const int button1Pin = 10; 
+const int ledPin =  13;
+
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+char request[] =  "{\"resource\":\"car1\",\"time\":1351824120,\"name\":\"fred\",\"assist\":\"monica\"}";
+
+void setup()
+{
+  Serial.begin(9600);//시리얼 통신 초기화
+  bluetooth.begin(9600);//블루투스 통신 초기화
+  pinMode(button1Pin, INPUT);
+  pinMode(ledPin, OUTPUT);  
+}
+ 
+ 
+void loop()
+{ 
+  int button1State;     
+  // 버튼이 눌렸는 지 아닌지 버튼의 상태를 읽어와서 앞에 선언한 변수에 저장
+  button1State = digitalRead(button1Pin);    
+  
+  if(button1State == LOW){         
+    bluetooth.print(request);
+    bluetooth.print("\n");
+  }
+  handle();
+}
+
+void handle(){
+  char response[128]={0};
+  int i = 0;
+  while(!bluetooth.available());
+  while(bluetooth.available()){
+    response[i] = bluetooth.read();
+    i++;
+  }
+  String result(response);
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(result);
+  boolean isSuccess = root["result"];
+  if (isSuccess) {
+    digitalWrite (ledPin, HIGH);
+    delay(1000);
+    digitalWrite (ledPin, LOW);
+  }
+}
+
