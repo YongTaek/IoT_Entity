36fcfca16e42d9be12e366bd103383c6430bf48a
오용택 <ka123ak1@gmail.com>
2017-08-25 21:05:08 +0900

    [08/25] bluetooth가 전송한 값에 따라 다른 led를 켜도록 변경

--- EntityWithBT/EntityWithBT.ino
+++ EntityWithBT/EntityWithBT.ino
@@ -4,38 +4,33 @@
 int bluetoothTx = 5;
 int bluetoothRx = 6;
 
-const int button1Pin = 10; 
-const int ledPin =  13;
+const int ledPin1 =  13;
+const int ledPin2 =  12;
+const int ledPin3 =  11;
 
 SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
-char request[] =  "{\"resource\":\"car1\",\"time\":1351824120,\"name\":\"fred\",\"assist\":\"monica\"}";
 
 void setup()
 {
   Serial.begin(9600);//시리얼 통신 초기화
   bluetooth.begin(9600);//블루투스 통신 초기화
-  pinMode(button1Pin, INPUT);
-  pinMode(ledPin, OUTPUT);  
+  pinMode(ledPin1, OUTPUT);  
+  pinMode(ledPin2, OUTPUT);  
+  pinMode(ledPin3, OUTPUT);  
+
 }
  
  
 void loop()
 { 
-  int button1State;     
-  // 버튼이 눌렸는 지 아닌지 버튼의 상태를 읽어와서 앞에 선언한 변수에 저장
-  button1State = digitalRead(button1Pin);    
-  
-  if(button1State == LOW){         
-    bluetooth.print(request);
-    bluetooth.print("\n");
-    handle();
-  }
+  handle();
 }
 
 void handle(){
   char response[128]={'\0'};
   int i = 0;
   while(!bluetooth.available());
+  
   while(bluetooth.available()){
     delay(10);
     response[i] = bluetooth.read();
@@ -45,11 +40,20 @@ void handle(){
   String result(response);
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.parseObject(result);
-  boolean isSuccess = root["result"];
-  if (isSuccess) {
-    digitalWrite (ledPin, HIGH);
-    delay(1000);
-    digitalWrite (ledPin, LOW);
+  int led = root["led"];
+  int time = root["time"];
+  if (led == 1) {
+    digitalWrite (ledPin1, HIGH);
+    delay(time);
+    digitalWrite (ledPin1, LOW);
+  } else if (led == 2) {
+    digitalWrite (ledPin2, HIGH);
+    delay(time);
+    digitalWrite (ledPin2, LOW);    
+  } else if (led == 3) {
+    digitalWrite (ledPin3, HIGH);
+    delay(time);
+    digitalWrite (ledPin3, LOW);    
   }
 }
 
